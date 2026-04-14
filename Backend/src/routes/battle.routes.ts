55477cf840b3e019d3b23f686a9d26c32779fc6e
vishveshshukla0008 <vishveshshukla82@gmail.com>
2026-04-14 15:02:31 +0530

    feat: create battle routes with authentication and validation middleware

--- Backend/src/routes/battle.routes.ts
+++ Backend/src/routes/battle.routes.ts
@@ -1,9 +1,10 @@
 import { Router, type Router as RouterType } from "express";
 import { battleValidation } from "../validations/battle.Validation.js";
 import { battleController } from "../controllers/battle.Controller.js";
+import { authUser } from "../middlewares/auth.middleware.js";
 
 const battleRouter: RouterType = Router();
 
-battleRouter.post("/", battleValidation, battleController);
+battleRouter.post("/", authUser, battleValidation, battleController);
 
 export default battleRouter;
